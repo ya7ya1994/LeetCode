f8fbe08c054e70a71a9f09f0cf922df3caba7a94
Yahya Abdul-Hakeem <42775710+ya7ya1994@users.noreply.github.com>
2020-04-23 12:49:54 -0700

    Optimized MergeIntervals

--- src/Leetcode/challenge/Algorithms/MergeIntervals.java
+++ src/Leetcode/challenge/Algorithms/MergeIntervals.java
@@ -11,14 +11,14 @@ public class MergeIntervals {
     }
 
     public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0 || intervals.length ==1)
+            return intervals;
         int index = 0;
         int counter = 0;
         Arrays.sort(intervals, Comparator.comparingDouble(o -> o[0]));
         for (int i = 0; i < counter+1; i++) {
             for (int j = index + 1; j < intervals.length; j++) {
-                if ((intervals[i][1] <= intervals[j][1] && intervals[i][1] >= intervals[j][0]) ||
-                        (intervals[i][1] >= intervals[j][1] && intervals[j][1] >= intervals[i][0])) {
-                    intervals[i][0] = Math.min(intervals[i][0], intervals[j][0]);
+                if (intervals[i][1] >= intervals[j][0]) {
                     intervals[i][1] = Math.max(intervals[i][1], intervals[j][1]);
                 } else {
                     intervals[i + 1][0] = intervals[j][0];
